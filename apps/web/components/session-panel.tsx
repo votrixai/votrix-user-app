0c44a05a9ac7adc9562c1a3eed97aacb9dc65a4b
yzh9810 <yuezonghan@gmail.com>
2026-05-02 17:39:06 -0700

    added all

--- apps/web/components/session-panel.tsx
+++ apps/web/components/session-panel.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState, useCallback } from "react";
 import { useRouter, useParams } from "next/navigation";
 import Link from "next/link";
 import {
+  Loader2,
   PanelLeftClose,
   Plus,
   Trash2,
